6736a1a01e5ad21825e4002e8acaf5f91b44937e
khalid-hegazy <Khaled.Hegazy@Andalusiagroup.net>
2026-07-17 17:32:32 +0300

    Delete src directory

--- src/config.py
+++
@@ -1,5 +0,0 @@
-
-FIREWORKS_API_KEY = "************************"
-LLM_MODEL = "accounts/fireworks/models/llama-v3p3-70b-instruct"
-EMBED_MODEL = "chan4lk/resume-ranking-model"
-QDRANT_PATH = "qdrant_cvs"  
